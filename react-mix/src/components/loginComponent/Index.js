3d1b166cab02187669c74daa252e547f52d2f532
Supuni22 <supunimanamperi@gmail.com>
2020-12-12 13:55:42 +0530

    Chnages made

--- react-mix/src/components/loginComponent/Index.js
+++ react-mix/src/components/loginComponent/Index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Login from './Login';
 
+//import './style.scss';
+// Tried a lot to install node-sass npm package to use scss with react. But it failed
+import './style.css';
 
 const Index = () => {
     return (
